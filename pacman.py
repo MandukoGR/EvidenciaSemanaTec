e72b43aff2607d5dba7a33fe88dafe488ac23554
Mike5801 <A01610836@itesm.mx>
2022-05-12 10:42:24 -0500

    Describe the methods and some lines of code as a comment

--- pacman.py
+++ pacman.py
@@ -7,12 +7,14 @@ path = Turtle(visible=False)
 writer = Turtle(visible=False)
 aim = vector(5, 0)
 pacman = vector(-40, -80)
+# The second vector of the array defines their initial speed, in other words, how fast they move on their first movement
 ghosts = [
     [vector(-180, 160), vector(5, 0)],
     [vector(-180, -160), vector(0, 5)],
     [vector(100, 160), vector(0, -5)],
     [vector(100, -160), vector(-5, 0)],
 ]
+# Defines in which tiles of the board can the ghosts and pacman move. (The ones that have the value 1)
 tiles = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
@@ -35,9 +37,8 @@ tiles = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 ]
-
+# Draw square using path at (x, y).
 def square(x, y):
-    "Draw square using path at (x, y)."
     path.up()
     path.goto(x, y)
     path.down()
@@ -48,16 +49,14 @@ def square(x, y):
         path.left(90)
 
     path.end_fill()
-
+# Return offset of point in tiles.
 def offset(point):
-    "Return offset of point in tiles."
     x = (floor(point.x, 20) + 200) / 20
     y = (180 - floor(point.y, 20)) / 20
     index = int(x + y * 20)
     return index
-
+# Return True if point is valid in tiles.
 def valid(point):
-    "Return True if point is valid in tiles."
     index = offset(point)
 
     if tiles[index] == 0:
@@ -69,10 +68,11 @@ def valid(point):
         return False
 
     return point.x % 20 == 0 or point.y % 20 == 0
-
+# Draw world using path.
 def world():
-    "Draw world using path."
+    # The method bgcolor will change the background color of the board
     bgcolor('black')
+    # The method color will change the color of the tiles that can be passed by
     path.color('blue')
 
     for index in range(len(tiles)):
@@ -86,10 +86,10 @@ def world():
             if tile == 1:
                 path.up()
                 path.goto(x + 10, y + 10)
+                # The next line of code will define the color of the "fruits" that pacman needs to eat
                 path.dot(2, 'white')
-
+# Move pacman and all ghosts.
 def move():
-    "Move pacman and all ghosts."
     writer.undo()
     writer.write(state['score'])
 
@@ -109,12 +109,13 @@ def move():
 
     up()
     goto(pacman.x + 10, pacman.y + 10)
+    # The next line of code will define the color of pacman and its size
     dot(20, 'yellow')
-
     for point, course in ghosts:
         if valid(point + course):
             point.move(course)
         else:
+            # The array "options" defines how many tiles the ghosts are moving after their first movement every 100 ms, namely, their speed
             options = [
                 vector(5, 0),
                 vector(-5, 0),
@@ -127,8 +128,8 @@ def move():
 
         up()
         goto(point.x + 10, point.y + 10)
+        # The next line of code will define the color of the ghosts
         dot(20, 'red')
-
     update()
 
     for point, course in ghosts:
@@ -136,24 +137,27 @@ def move():
             return
 
     ontimer(move, 100)
-
+# Change pacman aim if valid.
 def change(x, y):
-    "Change pacman aim if valid."
+    
     if valid(pacman + vector(x, y)):
         aim.x = x
         aim.y = y
-
+# The setup method sets the window size of the game
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
 writer.goto(160, 160)
 writer.color('white')
 writer.write(state['score'])
+# The listen method will receive the user's input
 listen()
+# The next four lines of code defines the movement of pacman according to the input
 onkey(lambda: change(5, 0), 'Right')
 onkey(lambda: change(-5, 0), 'Left')
 onkey(lambda: change(0, 5), 'Up')
 onkey(lambda: change(0, -5), 'Down')
+# The method world initializes the board
 world()
 move()
 done()
